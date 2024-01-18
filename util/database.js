ea8cd2495b8706fd5279e1938a914b12796a33a2
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2024-01-18 18:26:49 +0530

    first commit

---
+++ util/database.js
@@ -0,0 +1,28 @@
+const MongoClient = require("mongodb").MongoClient;
+
+let db;
+
+const connectMongoDB = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://bhuvaneshhj:Bhuvi@cluster0.fghogsf.mongodb.net/noSQL?retryWrites=true&w=majority"
+  )
+    .then((result) => {
+      console.log("Connected!");
+      db = result.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err)
+      throw err;  
+    });
+};
+
+const getDb = () =>{
+  if(db){
+    return db
+  }
+  throw "No database found!"
+}
+
+exports.connectMongoDB = connectMongoDB; 
+exports.getDb = getDb; 
